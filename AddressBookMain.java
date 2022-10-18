e70a1f66be0bb4f40bb3453deafd5edac6e9b091
pratikmgajbhiye <gajbhiyepratik2@gmail.com>
2022-10-18 22:15:24 +0530

    Added: Welcome to Address Book Program

---
+++ AddressBookMain.java
@@ -0,0 +1,14 @@
+package com.AddressBookProgram;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Welcome to Address Book Program");
+		
+	}
+
+}
