0e160226c9451106032b95096173a7228e4b7511
pratikmgajbhiye <gajbhiyepratik2@gmail.com>
2022-10-19 12:01:40 +0530

    Added: Ability to Edit Contact

---
+++ AdderssBook.java
@@ -0,0 +1,68 @@
+package com.AddressBookProgram;
+
+import java.util.Scanner;
+public class AdderssBook {
+	
+	Contacts contact;
+	Scanner sc =new Scanner(System.in);
+	public void addContacts() {
+		contact=new Contacts();
+		System.out.println("Enter the First Name : ");
+		String firstname=sc.nextLine();
+		
+		System.out.println("Enter the Last Name : ");
+		String lastname=sc.nextLine();
+		
+		System.out.println("Enter the Adderss : ");
+		String address=sc.nextLine();
+		
+		System.out.println("Enter the City : ");
+		String city=sc.nextLine();
+		
+		System.out.println("Enter the State : ");
+		String state=sc.nextLine();
+		
+		System.out.println("Enter the Zipcode : ");
+		String zip=sc.nextLine();
+		
+		System.out.println("Enter the Email ID : ");
+		String email=sc.nextLine();
+		
+		System.out.println("Enter the Phone No : ");
+		String phone_no=sc.nextLine();
+		
+		contact.setFirstname(firstname);
+        contact.setLastname(lastname);
+        contact.setAddress(address);
+        contact.setCity(city);
+        contact.setState(state);
+        contact.setZip(zip);
+        contact.setPhone_no(phone_no);
+        contact.setEmail(email);
+        
+        System.out.print(contact);
+	}
+	
+	public void displayContact() {
+		 System.out.print(contact);
+	}
+	
+	public void editContacts() {
+		System.out.println("Enter the First Name of a person to edit:");
+		String name = sc.nextLine();
+
+		if (name.equalsIgnoreCase(contact.getFirstname())) {
+			addContacts();
+		} else {
+			System.out.println("\nPlease Enter Valid Firstname: ");
+		editContacts();}
+
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -9,6 +9,36 @@ public class AddressBookMain {
 		Scanner sc =new Scanner(System.in);
 		System.out.println("Welcome to Address Book Program");
 		
+		AdderssBook obj=new AdderssBook();
+		int option;
+		boolean A=true;
+		
+		while(A) {
+			System.out.println("\n1.ADD CONTACT \n2.DISPLAY CONTACT \n3.EDIT CONTACT \n4.DELETE CONTACT \n");
+			System.out.println("Enter the Option Number");
+			option = sc.nextInt();
+			
+			switch (option) {
+			case 1:
+				obj.addContacts();
+				break;
+			case 2:
+				obj.displayContact();
+				break;
+			case 3:
+				obj.editContacts();
+				break;	
+			case 4:
+				obj.deleteContact();
+				break;
+			case 5:
+				A=false;
+				break;
+				}
+			
+			}
+		
+
 	}
 
 }
